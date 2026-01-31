ef25c3ea83c5ada83f719e448a0003f3f8cd6fde
mdmhrz <razufreelance@gmail.com>
2026-02-01 02:05:32 +0600

    duplicaiotn updated

--- src/module/auth/currentUser.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-const getCurrentUser = async (req: Request, res: Response) => {
-    const result = res.json(req.user);
-}
-
-export { getCurrentUser }
